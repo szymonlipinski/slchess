a6e1c75b5cc78461bbc69448ceac9a8bafdb3af4
Szymon Lipiński <mabewlun@gmail.com>
2020-09-17 10:30:38 +0200

    Initial import

---
+++ sources/config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#define PROJECT_NAME "slchess"
+
+#define PROJECT_VERSION "0.1.0"
+
+#define PROJECT_VERSION_MAJOR "0"
+#define PROJECT_VERSION_MINOR "1"
+#define PROJECT_VERSION_PATCH "0"
+
---
+++ sources/slchess.cpp
@@ -0,0 +1,8 @@
+//
+// Created by szymon on 17.09.2020.
+//
+
+int main() {
+
+}
+
